fee877a4cfa7851b3ec6df6143ca14c0b282a72f
Rezaul Karim <rkrohan94@gmail.com>
2026-05-08 16:18:41 +0600

    Fix return type from Htmlable to View

--- app/Filament/Pages/CustomLogin.php
+++ app/Filament/Pages/CustomLogin.php
@@ -3,11 +3,11 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Auth\Login as BaseLogin;
-use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View;
 
 class CustomLogin extends BaseLogin
 {
-    public function render(): Htmlable
+    public function render(): View
     {
         return view('filament.pages.auth.login');
     }
