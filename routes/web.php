1c07926d70a67fc1c46875e6de9e7ac779c71ab5
Rezaul Karim <rkrohan94@gmail.com>
2026-05-09 11:18:58 +0600

    Set Laravel health endpoint to /health for Railway

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\EmployeeProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', fn () => response()->json(['status' => 'ok']));
-
 Route::get('/', function () {
     return redirect('/admin');
 });
